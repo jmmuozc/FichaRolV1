b41bc5ea7dd3eaf8fa1e47ee5f3cb0938fc2dbd9
Jaime <jaimepovedafernandez@gmail.com>
2023-02-05 04:26:15 +0100

    selects raza y clase

--- js/script.js
+++ js/script.js
@@ -6,12 +6,9 @@ let valoresInput = document.getElementById("valores");
 let sumaDiv = document.getElementById("sumaDiv");
 let sumaSpan = document.getElementById("sumaSpan");
 
-let fueSelect = document.getElementById("fueSelect");
-let desSelect = document.getElementById("desSelect");
-let conSelect = document.getElementById("conSelect");
-let carSelect = document.getElementById("carSelect");
-let intSelect = document.getElementById("intSelect");
-let sabSelect = document.getElementById("sabSelect");
+let mejoraRazaDiv = document.getElementById("mejoraRazaDiv");
+let mejoraClaseDiv = document.getElementById("mejoraClaseDiv");
+let levelSelect = document.getElementById("levelSelect");
 
 let valores = [];
 function dado() {
@@ -66,6 +63,7 @@ function rellenarSelects() {
     for (let i = 0; i < selects.length; i++) {
         let emptyOption = document.createElement("option");
         emptyOption.value = "";
+        emptyOption.innerText = "";
 
         selects[i].innerHTML = "";
         selects[i].appendChild(emptyOption);
@@ -111,4 +109,80 @@ function actualizarValoresSelec(opt) {
             }
         }
     }
+}
+
+function cambioRaza(opt) {
+    let div = document.createElement("div");
+    mejoraRazaDiv.innerHTML = "";
+    switch (opt.value) {
+        case "Humano":
+            div.innerHTML = '<b>Mejora de Raza: </b>  <div class="w-auto m-auto"><div class="input-group w-auto" style="width: 140px;"><span class="input-group-text" style="width:50px;">+1</span><select class="form-select" aria-label="DES"><option value=""></option><option value="FUE">FUE</option><option value="DES">DES</option><option value="CON">CON</option><option value="CAR">CAR</option><option value="INT">INT</option><option value="SAB">SAB</option></select></div></div>  <div class="w-auto m-auto"><div class="input-group w-auto" style="width: 140px;"><span class="input-group-text" style="width:50px;">+1</span><select class="form-select" aria-label="DES"><option value=""></option><option value="FUE">FUE</option><option value="DES">DES</option><option value="CON">CON</option><option value="CAR">CAR</option><option value="INT">INT</option><option value="SAB">SAB</option></select></div></div>';
+            break;
+        case "Elfo":
+            div.innerHTML = "<b>Mejora de Raza: </b> +1 Des y +1 Int";
+            break;
+        case "Enano":
+            div.innerHTML = "<b>Mejora de Raza: </b> +1 Fue y +1 Con";
+            break;
+        case "Mediano":
+            div.innerHTML = "<b>Mejora de Raza: </b> +1 Des y +1 Car";
+            break;
+        case "Goblin":
+            div.innerHTML = "<b>Mejora de Raza: </b> +1 Des y +1 Sab";
+            break;
+        case "Orco":
+            div.innerHTML = "<b>Mejora de Raza: </b> +1 Fue y +1 Con";
+            break;
+        case "Reptiliano":
+            div.innerHTML = "<b>Mejora de Raza: </b> +1 Fue y +1 Car";
+            break;
+        case "Otra":
+
+            break;
+
+        default:
+            div.innerHTML = "<b>Mejora de Raza: </b>";
+            break;
+    }
+
+    mejoraRazaDiv.appendChild(div);
+}
+
+function cambioClase(opt) {
+    let div = document.createElement("div");
+    mejoraClaseDiv.innerHTML = "";
+
+    switch (opt.value) {
+        case "Guerrero":
+            div.innerHTML = "<b>Pasiva de Clase (Sediento): </b>Al eliminar a un objetivo, gana una acción adicional.";
+            break;
+        case "Bárbaro":
+            div.innerHTML = "<b>Pasiva de Clase (Ansia): </b>Cuando tiene un arma en cada mano, puede atacar con las dos armas a la vez en el mismo ataque. De esta forma, lanzaría 2d4+FUE.";
+            break;
+        case "Pícaro":
+            div.innerHTML = "<b>Pasiva de Clase (Sombra): </b>Puedes gastar una acción para entrar en sigilo. Al salir de sigilo, hace un ataque crítico.";
+            break;
+        case "Bardo":
+            div.innerHTML = "<b>Pasiva de Clase (Canción Grupal): </b>Si realizas un hechizo sobre un aliado, puedes aplicar el mismo efecto a otro aliado que se encuentre adyacente al primero.";
+            break;
+        case "Cazador":
+            div.innerHTML = "<b>Pasiva de Clase (Hábil tirador): </b>Si atacas sin haberte movido, ganas +1 al daño.";
+            break;
+        case "Mago":
+            div.innerHTML = "<b>Pasiva de Clase (Maná): </b>Por cada hechizo lanzado, acumula un punto de maná. Los puntos de maná se pueden consumir para sumar +1 al daño de un ataque. Se pueden acumular un máximo de siete puntos de maná.";
+            break;
+        case "Clérigo":
+            div.innerHTML = "<b>Pasiva de Clase (Mi religión me lo permite): </b>Cuando lanza un hechizo, otorga +1 de daño a un aliado cercano.";
+            break;
+
+        default:
+            div.innerHTML = "<b>Pasiva de Clase : </b>";
+            break;
+    }
+
+    mejoraClaseDiv.appendChild(div);
+}
+
+function cambioNivel(){
+
 }
